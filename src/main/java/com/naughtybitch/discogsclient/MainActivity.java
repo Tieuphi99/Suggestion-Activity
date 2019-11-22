9b0ce59a202bd37aa7d2dc32aa3e9e5b69feaf77
Tieuphi99 <tieuphi99@gmail.com>
2019-11-22 19:35:03 +0700

    Suggestion Activity

---
+++ src/main/java/com/naughtybitch/discogsclient/MainActivity.java
@@ -0,0 +1,197 @@
+package com.naughtybitch.discogsclient;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity
+        implements HomeFragment.OnFragmentInteractionListener,
+        MarketFragment.OnFragmentInteractionListener,
+        SettingsFragment.OnFragmentInteractionListener {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+    private MenuItem searchItem;
+
+    // Declare Variables
+    ListView list;
+    SearchAdapter adapter;
+    SearchView editSearch;
+    String[] searchQueries;
+    ArrayList<SearchQuery> arrayList = new ArrayList<SearchQuery>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Custom ActionBar
+        final Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        // TODO: Implement into SearchableActivity
+        searchQueries = new String[]{"TextView", "ListView", "SearchView",
+                "RatingBar", "ToolBar", "Button", "EditText", "ToggleButton",
+                "ImageView", "SlidingDrawer", "Android"};
+
+        list = (ListView) findViewById(R.id.list_view);
+        for (String searchQuery : searchQueries) {
+            SearchQuery searchQuery1 = new SearchQuery(searchQuery);
+            // Binds all strings into an array
+            arrayList.add(searchQuery1);
+        }
+
+        adapter = new SearchAdapter(this, arrayList);
+
+        // DrawerLayout
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+        }
+
+        // Create a new Fragment to be placed in the activity layout
+        HomeFragment firstFragment = HomeFragment.newInstance();
+
+        // In case this activity was started with special instructions from an
+        // Intent, pass the Intent's extras to the fragment as arguments
+        firstFragment.setArguments(getIntent().getExtras());
+
+        // Add the fragment to the 'fragment_container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                drawerLayout.closeDrawers();
+                switch (id) {
+                    case R.id.home:
+                        navigateToFragment(HomeFragment.newInstance());
+                        Toast.makeText(MainActivity.this, "HomeFragment", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.market:
+                        navigateToFragment(MarketFragment.newInstance());
+                        Toast.makeText(MainActivity.this, "MarketFragment", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.settings:
+                        navigateToFragment(SettingsFragment.newInstance());
+                        Toast.makeText(MainActivity.this, "SettingsFragment", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (toggle.onOptionsItemSelected(item)) return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START))
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        else super.onBackPressed();
+    }
+
+    @Override
+    public void onAccountFragmentInteraction(Uri uri) {
+        // Do stuff
+    }
+
+    @Override
+    public void onSettingsFragmentInteraction(Uri uri) {
+        // Do different stuff
+    }
+
+    @Override
+    public void onCartFragmentInteraction(Uri uri) {
+        // Do something
+    }
+
+    private void navigateToFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+
+        // TODO: Implement into SearchableActivity
+        searchItem = menu.findItem(R.id.search_bar);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint(getResources().getText(R.string.search_hint));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.filter(newText);
+                return false;
+            }
+        });
+
+        return true;
+    }
+}
+
---
+++ src/main/java/com/naughtybitch/discogsclient/SuggestionsFragment.java
@@ -0,0 +1,50 @@
+package com.naughtybitch.discogsclient;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SuggestionsFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SuggestionsFragment() {
+        // Required empty public constructor
+    }
+    public static SuggestionsFragment newInstance(String param1, String param2) {
+        SuggestionsFragment fragment = new SuggestionsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_suggestions, container, false);
+    }
+}
